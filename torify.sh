7bd4a9390ad27f97c8087cb6d46ac1db09505774
Glenn <hello@glennsorrentino.com>
2023-04-14 16:02:45 -0700

    Update torify.sh

--- torify.sh
+++ torify.sh
@@ -19,22 +19,34 @@ sleep 10
 onion_address=$(sudo cat /var/lib/tor/hidden_service/hostname)
 
 # Edit mediawiki-onion.conf with the onion address
-sudo bash -c "cat > /etc/apache2/sites-available/mediawiki-onion.conf << EOL
-<VirtualHost *:80>
-    ServerName ${onion_address}
-    DocumentRoot /var/www/html/mediawiki
+sudo bash -c "cat > /etc/nginx/sites-available/mediawiki-onion << EOL
+server {
+    listen 80;
+    server_name ${onion_address};
 
-    <Directory /var/www/html/mediawiki/>
-        Options FollowSymLinks
-        AllowOverride All
-        Require all granted
-    </Directory>
-</VirtualHost>
+    root /var/www/html/mediawiki;
+    index index.php;
+
+    location / {
+        try_files \$uri \$uri/ @mediawiki;
+    }
+
+    location @mediawiki {
+        rewrite ^/([^?]*)(?:\?(.*))? /index.php?title=\$1&\$2 last;
+    }
+
+    location ~ \.php$ {
+        include snippets/fastcgi-php.conf;
+        fastcgi_pass unix:/var/run/php/php7.4-fpm.sock;
+        fastcgi_param SCRIPT_FILENAME \$document_root\$fastcgi_script_name;
+        include fastcgi_params;
+    }
+}
 EOL"
 
-# Enable the mediawiki-onion site and reload the Apache configuration
-sudo a2ensite mediawiki-onion
-sudo systemctl reload apache2
+# Enable the mediawiki-onion site and reload the Nginx configuration
+sudo ln -s /etc/nginx/sites-available/mediawiki-onion /etc/nginx/sites-enabled/
+sudo systemctl reload nginx
 
 # Print the onion service address
 echo "Your MediaWiki onion service is available at: ${onion_address}"
