1724698cae0e932de1a44165850ec5bcb48e72f9
Glenn <hello@glennsorrentino.com>
2023-04-14 15:40:06 -0700

    Update torify.sh

--- torify.sh
+++ torify.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# Install Tor
 sudo apt install -y tor
 
 # Configure Tor for onion service
@@ -14,6 +15,27 @@ sudo systemctl restart tor
 # Wait for the onion service to be created
 sleep 10
 
-# Get and print the onion service address
+# Get the onion service address
 onion_address=$(sudo cat /var/lib/tor/hidden_service/hostname)
+
+# Edit mediawiki-onion.conf with the onion address
+sudo bash -c "cat > /etc/apache2/sites-available/mediawiki-onion.conf << EOL
+<VirtualHost *:80>
+    ServerName ${onion_address}
+    ServerAlias *.onion
+    DocumentRoot /var/www/html/mediawiki
+
+    <Directory /var/www/html/mediawiki/>
+        Options FollowSymLinks
+        AllowOverride All
+        Require all granted
+    </Directory>
+</VirtualHost>
+EOL"
+
+# Enable the mediawiki-onion site and reload the Apache configuration
+sudo a2ensite mediawiki-onion
+sudo systemctl reload apache2
+
+# Print the onion service address
 echo "Your MediaWiki onion service is available at: ${onion_address}"
