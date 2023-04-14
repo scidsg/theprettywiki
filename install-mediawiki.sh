692c46cbc81cb3f204a61ca1d3f74fab97d815af
Glenn <hello@glennsorrentino.com>
2023-04-14 11:54:38 -0700

    Update install-mediawiki.sh

--- install-mediawiki.sh
+++ install-mediawiki.sh
@@ -1,239 +1,62 @@
-# Back up Vector Skin
-cd /var/www/html/mediawiki/skins
-cp -r Vector/ Vector-Backup/
+#!/bin/bash
 
-# Back up current less file
-cd Vector/resources/skins.vector.styles/
-cp skin.less old-skin.less
-cat > skin.less << EOL
-/**
- * Vector modern stylesheets
- * See '../common/common.less' for common screen and print Vector stylesheets.
- */
+# Update the system
+sudo apt update
+sudo apt -y dist-upgrade
+sudo apt -y autoremove
 
-@import '../common/variables.less';
-@import 'mediawiki.mixins.less';
+# Install necessary packages
+sudo apt install -y apache2 php libapache2-mod-php mariadb-server php-mysql php-xml php-mbstring php-apcu php-intl imagemagick php-gd php-cli curl php-curl git whiptail
 
-@media screen {
-        // Layouts
-        @import './layouts/screen.less';
+# Get the latest MediaWiki version and tarball URL
+MW_TARBALL_URL=$(curl -s https://www.mediawiki.org/wiki/Download | grep -oP '(?<=href=")[^"]+(?=\.tar\.gz")' | head -1)
+MW_VERSION=$(echo $MW_TARBALL_URL | grep -oP '(?<=mediawiki-)[^/]+')
 
-        // Components
-        @import './components/ArticleToolbar.less';
-        @import './components/SearchBoxLoader.less';
-        @import './components/VueEnhancedSearchBox.less';
-        @import './components/Sidebar.less';
-        @import './components/LanguageButton.less';
-        @import './components/UserLinks.less';
-        @import './components/Header.less';
-        @import './components/Footer.less';
-        @import './components/MenuDropdown.less';
-        @import './components/MenuTabs.less';
-        @import './components/MenuPortal.less';
-        @import './components/StickyHeader.less';
-        @import './components/TabWatchstarLink.less';
-        @import './components/TableOfContents.less';
-        @import './components/TableOfContentsCollapsed.less';
+# Download MediaWiki
+wget -O mediawiki-${MW_VERSION}.tar.gz "${MW_TARBALL_URL}.tar.gz"
+tar xvzf mediawiki-${MW_VERSION}.tar.gz
 
-        // Custom Styles
-        @import './custom/custom.less';
-}
+# Move MediaWiki to the web server directory
+sudo mv mediawiki-${MW_VERSION} /var/www/html/mediawiki
 
-@media all {
-        // Component styles that should apply in all media.
-        @import './components/Logo.less';
-}
+# Set appropriate permissions
+sudo chown -R www-data:www-data /var/www/html/mediawiki
+sudo chmod -R 755 /var/www/html/mediawiki
 
-@media print {
-        @import './layouts/print.less';
-}
+# Enable Apache rewrite module
+sudo a2enmod rewrite
 
-@import './layouts/gradeC.less';
-EOL
+# Create a MediaWiki Apache configuration file
+sudo bash -c 'cat > /etc/apache2/sites-available/mediawiki.conf << EOL
+<VirtualHost *:80>
+    ServerName mediawiki.local
+    DocumentRoot /var/www/html/mediawiki
 
-# Create custom CSS
-mkdir custom
-# Create a systemd service
-cat > custom/custom.less << EOL
-body {
-  background-color: white;
-  color: #333;
-}
+    <Directory /var/www/html/mediawiki/>
+        Options FollowSymLinks
+        AllowOverride All
+        Require all granted
+    </Directory>
+</VirtualHost>
+EOL'
 
-.mw-logo-wordmark {
-  font-family: 'Linux Libertine','Georgia','Times',serif;
-  font-size: 1.25rem;
-  font-weight: normal;
-}
+# Enable the MediaWiki site and disable the default site
+sudo a2ensite mediawiki
+sudo a2dissite 000-default
 
-.mw-header {
-  background-color: white;
-  top: 0;
-  display: flex;
-  justify-self: stretch;
-  justify-content: space-between;
-}
+# Restart Apache
+sudo systemctl restart apache2
 
-.mw-body h1, .mw-body-content h1 {
-  font-size: 3rem;
-}
+# Get user input using whiptail
+db_name=$(whiptail --inputbox "Please enter your desired database name for MediaWiki:" 8 78 --title "Database Name" 3>&1 1>&2 2>&3)
+db_user=$(whiptail --inputbox "Please enter your desired database username for MediaWiki:" 8 78 --title "Database Username" 3>&1 1>&2 2>&3)
+db_pass=$(whiptail --passwordbox "Please enter your desired database password for MediaWiki:" 8 78 --title "Database Password" 3>&1 1>&2 2>&3)
 
-h1, h2, h3, h4, h5, h6 {
-  color: #333;
-}
+# Create the database and user
+sudo mysql -e "CREATE DATABASE \`${db_name}\` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;"
+sudo mysql -e "CREATE USER '${db_user}'@'localhost' IDENTIFIED BY '${db_pass}';"
+sudo mysql -e "GRANT ALL PRIVILEGES ON \`${db_name}\`.* TO '${db_user}'@'localhost';"
+sudo mysql -e "FLUSH PRIVILEGES;"
 
-h1, h2 {
-  border-bottom: 1px solid rgba(0,0,0,0.1);
-  margin: 2rem 0 1rem 0;
-}
-
-.vector-body p {
-  max-width: 640px;
-  font-size: 1rem;
-  line-height: 1.6;
-  margin: 1rem 0;
-}
-
-.mw-header-content, .mw-header-aside {
-  justify-content: flex-end;
-}
-
-.client-js .vector-search-box-vue.vector-search-box-show-thumbnail.vector-search-box-auto-expand-width .vector-search-box-input {
-  margin-left: 0;
-  width: 100%;
-  max-width: 320px;
-}
-
-a {
-  color: #333 !important;
-}
-
-a.extiw:visited, .mw-parser-output a.external:visited {
-  color: #333;
-}
-
-#mw-content-text a {
-  text-decoration: underline;
-}
-
-.mw-body .firstHeading {
-  padding-bottom: 1rem;
-  margin-bottom: 0;
-}
-
-.cdx-search-input--has-end-button {
-  border: 1px solid rgba(0,0,0,0.1);
-  border-radius: .25rem;
-}
-
-.cdx-text-input__input:enabled {
-  border: 1px solid rgba(0,0,0,0.1);
-  border-radius: .25rem;
-}
-
-.cdx-text-input__input:enabled:hover {
-  border-color: rgba(0,0,0,0.1);
-}
-
-.cdx-typeahead-search--show-thumbnail.cdx-typeahead-search--auto-expand-width:not(.cdx-typeahead-search--active) {
-  margin-left: 0;
-}
-
-.vector-body h2 {
-  font-size: 2rem;
-  padding-bottom: .5rem;
-}
-
-.vector-body h3 {
-  font-size: 1.5rem;
-}
-
-.vector-menu-tabs .mw-list-item.selected a, .vector-menu-tabs .mw-list-item.selected a:visited {
-  color: #333;
-  border-bottom: 3px solid black;
-}
-
-.vector-menu-tabs .mw-list-item > a, .mw-article-toolbar-container .vector-menu-dropdown > a, .vector-menu-tabs .mw-list-item .vector-menu-heading, .mw-article-toolbar-container .vector-menu-dropdown .vector-menu-heading {
-  padding: 1rem;
-  max-height: fit-content;
-}
-
-.vector-menu-tabs .mw-list-item, .mw-article-toolbar-container .vector-menu-dropdown {
-  margin: 0;
-}
-
-#left-navigation {
-  margin-left: 0;
-}
-
-.vector-page-titlebar, 
-.vector-page-toolbar-container, 
-.mw-article-toolbar-container {
-  box-shadow: none;
-  border-bottom: 1px solid rgba(0,0,0,0.1);
-}
-
-.mw-table-of-contents-container .sidebar-toc {
-  position: absolute;
-}
-
-.vector-toc-collapse-button {
-  display: none;
-}
-
-#mw-sidebar-button::before {
-  background-image: url("data:image/svg+xml,%3Csvg xmlns=%22http://www.w3.org/2000/svg%22 width=%2220%22 height=%2220%22 viewBox=%220 0 20 20%22%3E %3Ctitle%3E menu %3C/title%3E %3Cpath d=%22M1 3v2h18V3zm0 8h18V9H1zm0 6h18v-2H1z%22/%3E %3C/svg%3E");
-}
-
-.mw-sidebar {
-  background-color: white;
-  border: 1px solid rgba(0,0,0,0.1);
-  border-radius: .325rem;
-  box-shadow: 0 10px 1.5rem -1rem rgba(0,0,0,0.2);
-  max-width: fit-content;
-  position: fixed;
-  z-index: 2;
-  top: 4.5rem;
-}
-
-.vector-toc-not-collapsed #mw-sidebar-checkbox:not(:checked) ~ .mw-table-of-contents-container .vector-sticky-toc-container {
-  margin-top: 0;
-}
-
-.vector-menu-portal .vector-menu-content ul {
-  padding-top: 0;
-}
-
-.vector-menu-portal .vector-menu-content li {
-  margin: .5rem 0;
-}
-
-.vector-menu-portal .vector-menu-heading {
-  font-size: .825rem;
-  font-weight: bold;
-  color: #333;
-}
-
-@media only screen and (max-width: 999px) {
-  .mw-table-of-contents-container .sidebar-toc {
-    position: absolute !important;  
-    top: 6rem;
-    right: 1rem;
-    left: inherit;
-    background-color: white;
-    border: 1px solid rgba(0,0,0,0.1);
-    border-radius: .325rem;
-    box-shadow: 0 10px 1.5rem -1rem rgba(0,0,0,0.2);
-    padding: 1.5rem 1.5rem 1.5rem 2rem;
-  }
-
-  .vector-below-page-title #vector-toc-collapsed-button {
-    display: none;
-  }
-
-  #vector-toc-collapsed-button {
-    float: right;
-    transform: translateY(50%);
-  }
-}
-EOL
+# Done
+echo "MediaWiki has been installed. Please visit http://mediawiki.local to complete the setup and apply the Vector skin."
