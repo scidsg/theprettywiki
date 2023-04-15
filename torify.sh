361804cde41a29befd4f198c9c6938366fc1a411
Glenn <hello@glennsorrentino.com>
2023-04-14 20:39:02 -0700

    Update torify.sh

--- torify.sh
+++ torify.sh
@@ -38,6 +38,27 @@ sudo a2ensite mediawiki_onion
 sudo systemctl restart apache2
 sudo systemctl restart tor
 
+# Route network traffic through Tor
+# Install torsocks
+sudo apt install -y torsocks
+
+# Configure Tor for routing all network traffic
+sudo bash -c 'cat >> /etc/tor/torrc << EOL
+VirtualAddrNetworkIPv4 10.192.0.0/10
+AutomapHostsOnResolve 1
+TransPort 9040
+DNSPort 5353
+EOL'
+
+# Restart the Tor service
+sudo systemctl restart tor
+
+sudo iptables -t nat -A OUTPUT -p tcp -d 10.0.0.0/8 -j RETURN
+sudo iptables -t nat -A OUTPUT -p tcp -d 172.16.0.0/12 -j RETURN
+sudo iptables -t nat -A OUTPUT -p tcp -d 192.168.0.0/16 -j RETURN
+sudo iptables -t nat -A OUTPUT -p tcp -j REDIRECT --to-ports 9040
+sudo iptables -t nat -A OUTPUT -p udp --dport 53 -j REDIRECT --to-ports 5353
+
 # Retrieve the onion service hostname
 sleep 10
 onion_hostname=$(sudo cat /var/lib/tor/mediawiki_onion_service/hostname)
@@ -46,5 +67,27 @@ onion_hostname=$(sudo cat /var/lib/tor/mediawiki_onion_service/hostname)
 sudo bash -c "echo -e \"\\\$wgServer = 'http://$onion_hostname';\" >> /var/www/html/mediawiki/LocalSettings.php"
 
 # Display the onion service URL
-echo "Your MediaWiki site is now accessible exclusively as a Tor onion service at the following URL:"
-echo "http://${onion_hostname}"
+function display_service_status() {
+    local tor_status=$(systemctl is-active tor)
+    local apache_status=$(systemctl is-active apache2)
+
+    if [[ $tor_status == "active" && $apache_status == "active" ]]; then
+        echo -e "\033[32m●\033[0m The MediaWiki Tor onion service is running."
+    else
+        echo -e "\033[31m●\033[0m The MediaWiki Tor onion service is not running."
+    fi
+
+    local onion_hostname=$(sudo cat /var/lib/tor/mediawiki_onion_service/hostname 2>/dev/null)
+    if [[ -n $onion_hostname ]]; then
+        echo "http://${onion_hostname}"
+    else
+        echo "Onion address not found."
+    fi
+}
+
+# Wait a few seconds for the onion service to start
+sleep 5
+display_service_status
+
+# Display status on login
+echo 'display_service_status' >> ~/.bashrc
