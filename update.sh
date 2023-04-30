309755841a51b01669716c7567458bb4b153422e
Glenn Sorrentino <glenn.sorrentino@proton.me>
2023-04-30 14:36:41 -0700

    Update update.sh

--- update.sh
+++ update.sh
@@ -10,10 +10,10 @@ cd /var/www/html/mediawiki/skins/Vector/resources/skins.vector.styles/custom/
 
 mkdir -p archive/
 timestamp=$(date +%Y%m%d%H%M%S)
-mv custom.less archive/custom.less.$timestamp
-mv ddos.less archive/ddos.less.$timestamp
-mv homepage.css archive/homepage.css.$timestamp
-mv homepage.js archive/homepage.js.$timestamp
+mv custom.less archive/$timestamp-custom.less
+mv ddos.less archive/$timestamp-ddos.less
+mv homepage.css archive/$timestamp-homepage.css
+mv homepage.js archive/$timestamp-homepage.js
 
 wget https://raw.githubusercontent.com/scidsg/the-pretty-wiki/main/skin/custom.less
 wget https://raw.githubusercontent.com/scidsg/the-pretty-wiki/main/skin/ddos.less
